57593a65cbd7f564e0528be8b9caed9e88203ede
Christian Kubelle <ckubelle@stevens.edu>
2021-10-15 14:14:09 -0400

    Anomaly fix for US08

--- test_user_stories/test_user_story_8.py
+++ test_user_stories/test_user_story_8.py
@@ -10,19 +10,19 @@ from user_stories import user_story_8
 class TestUserStory8(unittest.TestCase):
 
     def test_birth_before_marriage_of_parentsA(self):
-        self.assertTrue(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), 'NA', date(2000, 10, 10)))
+        self.assertTrue(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), 'NA'))
 
     def test_birth_before_marriage_of_parentsB(self):
-        self.assertTrue(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), date(1980, 10, 10), 'NA'))
+        self.assertTrue(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), date(1980, 10, 10)))
 
     def test_birth_before_marriage_of_parentsC(self):
-        self.assertFalse(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), date(1950, 10, 10), 'NA'))
+        self.assertFalse(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), date(1950, 10, 10)))
 
-    def test_birth_before_marriage_of_parentsD(self):
-        self.assertTrue(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), date(1950, 10, 10), date(1967, 1, 9)))
+    def test_birth_before_divorce_of_parentsD(self):
+        self.assertTrue(user_story_8.birthBeforeDivorceCheck(date(1967, 10, 10), date(1967, 1, 9)))
 
-    def test_birth_before_marriage_of_parentsE(self):
-        self.assertFalse(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), date(1950, 10, 10), date(1967, 1, 11)))
+    def test_birth_before_divorce_of_parentsE(self):
+        self.assertFalse(user_story_8.birthBeforeDivorceCheck(date(1967, 10, 10),  date(1967, 1, 11)))
 
-    def test_birth_before_marriage_of_parentsF(self):
-                self.assertFalse(user_story_8.birthBeforeMarriageCheck(date(1967, 10, 10), date(1950, 10, 10), date(1970,10,10)))
+    def test_birth_before_divorce_of_parentsF(self):
+                self.assertFalse(user_story_8.birthBeforeDivorceCheck(date(1967, 10, 10),  date(1970,10,10)))
