efc03be50090ae37e5d9e1a61d4146beb07552ed
amohan00 <arm198@pitt.edu>
2021-10-19 09:18:46 -0400

    created us16 test file

---
+++ test_user_stories/test_user_story_16.py
@@ -0,0 +1,15 @@
+# User Story 16 Test Class
+# All males in a family should have the same last name
+
+import unittest
+from datetime import date
+from user_stories import user_story_16
+
+class TestUserStory16(unittest.TestCase):
+  def test_is_male_last(self):
+    
+  def test_check_male_last(self):
+  
+  def test_is_male(self):
+    
+  def find_last(self):
