d858453fcc3ef669dcd3a2e6272bbb61b700b91e
Leena Domadia <leena.domadia2018@gmail.com>
2021-10-21 21:48:36 -0400

    completed user story 13

--- user_stories/user_story_13.py
+++ user_stories/user_story_13.py
@@ -23,7 +23,23 @@ def isCorrectSibilingSpacing(bday_1, bday_2):
             return True
     return False
 
-def sibilingSpacing(fam_list, indi_list):
+def sibilingSpacing(indi_list, fam_list):
     error_statements = []
-
+    for fam in fam_list:
+        child_list = []
+        for child in fam['children']:
+            for indi in indi_list:
+                if child == indi['id']:
+                    child_list.append(indi)
+        # COMMENTED CODE PRINTED ERRORS TWICE (child1 & child2, child2 & child1)
+        # for child1 in child_list:
+        #     for child2 in child_list:
+        #         if child1 != child2:
+        #             if not isCorrectSibilingSpacing(child1['birthday'], child2['birthday']):
+        #                 error_statements.append(f"Error US13: {child1['name']} ({child1['id']}) and {child2['name']} ({child2['id']}) are born within 8 months of each other and are not twins")
+        for i in range(len(child_list)):
+            for j in range(i + 1, len(child_list)):
+                if not isCorrectSibilingSpacing(child_list[i]['birthday'], child_list[j]['birthday']):
+                    error_statements.append(f"Error US13: {child_list[i]['name']} ({child_list[i]['id']}) and {child_list[j]['name']} ({child_list[j]['id']}) are born within 8 months of each other and are not twins")
+        child_list = []
     return error_statements
