f0cc8fe4f7634e7b9fe193d1cdaaf257fb91e3e4
Leena Domadia <leena.domadia2018@gmail.com>
2021-10-21 21:02:51 -0400

    added function to check sibiling spacing

--- user_stories/user_story_13.py
+++ user_stories/user_story_13.py
@@ -6,17 +6,24 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 def isCorrectSibilingSpacing(bday_1, bday_2):
+    greater_bday = ''
+    lesser_bday = ''
+
     if bday_1 > bday_2:
-        if bday_2 + relativedelta(months=8) < bday_1:
+        greater_bday = bday_1
+        lesser_bday = bday_2
+    else:
+        greater_bday = bday_2
+        lesser_bday = bday_1
+    
+    if lesser_bday + relativedelta(months=8) < greater_bday:
+        return True
+    else:
+        if lesser_bday + relativedelta(days=2) > greater_bday:
             return True
+    return False
 
 def sibilingSpacing(fam_list, indi_list):
     error_statements = []
-    for fam in fam_list:
-        child_list = fam['child']
-        child_id_list = []
-        for child_id in child_list:
-            child_id_list.append(child_id)
-
 
     return error_statements
