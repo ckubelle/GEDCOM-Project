f6b6fb9106d89a5511733024980cecae3f40d9d5
amohan00 <arm198@pitt.edu>
2021-10-27 02:14:22 -0400

    finished checkmarr method and us10

--- user_stories/user_story_10.py
+++ user_stories/user_story_10.py
@@ -4,10 +4,15 @@
 import datetime
 
 # return error statements if marriage occurs before age 14
-def checkMarrAfter14(indi_list, fam_list):
+def checkMarrBefore14(indi_list, fam_list):
+    errorStatements = []
+    for fam in fam_list:
+        if (isMarrBefore14(indi_list, fam)):
+            errorStatements.append("Error US10: Marriage date in Family %s occurs before both spouses are over 14 years of age." % (fam["id"]))
+    return errorStatements
 
-# return true if marriage in given fam occurred after spouses were age 14
-def isMarrAfter14(indi_list, fam):
+# return true if marriage in given fam occurred before spouses were age 14
+def isMarrBefore14(indi_list, fam):
     # if no marriage, return false
     if fam["married"] == "NA":
         return false;
@@ -17,11 +22,11 @@ def isMarrAfter14(indi_list, fam):
         # get the spouses birthdays
         husbDate = getBirthday(indi_list, fam["husb_id"])
         wifeDate = getBirthday(indi_list, fam["wife_id"])
-        # if difference between birthday and marriage date is less than 14, false
-        if diffDate(marrDate, husbDate) < 14 or diffDate(marrDate, wifeDate) < 14:
-            return false
-        else:
+        # if difference between birthday and marriage date is less than 14, true
+        if diffDate(marrDate, husbDate) < 14.00 or diffDate(marrDate, wifeDate) < 14.00:
             return true
+        else:
+            return false
         
 # return difference in given dates by years
 def diffDates(date1, date2):
