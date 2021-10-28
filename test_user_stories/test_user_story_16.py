2f467f9c4319ce8852b8979de56878730e458a8b
amohan00 <arm198@pitt.edu>
2021-10-28 17:21:24 -0400

    final syntax/bug fixes

--- test_user_stories/test_user_story_16.py
+++ test_user_stories/test_user_story_16.py
@@ -6,7 +6,7 @@ import datetime
 from user_stories import user_story_16
 
 class TestUserStory16(unittest.TestCase):
-  # define objects to interact with during tests
+  # create test objects to interact with
   test_true = [
         {'id': '@I01@', 'name': 'Joseph /Biden/', 'gender': 'M', 'birthday': datetime.date(1942, 11, 20), 'age': 78, 'alive': True, 'death': 'NA', 'child': set(), 'spouse': {'@F01@'}},
         {'id': '@I02@', 'name': 'Neilia /Hunter/', 'gender': 'F', 'birthday': datetime.date(1942, 11, 28), 'age': 78, 'alive': True, 'death': 'NA', 'child': set(), 'spouse': {'@F01@'}},
@@ -17,9 +17,8 @@ class TestUserStory16(unittest.TestCase):
         {'id': '@I02@', 'name': 'Neilia /Hunter/', 'gender': 'F', 'birthday': datetime.date(1942, 11, 28), 'age': 78, 'alive': True, 'death': 'NA', 'child': set(), 'spouse': {'@F01@'}},
         {'id': '@I03@', 'name': 'James /Jacobs/', 'gender': 'M', 'birthday': datetime.date(1969, 2, 3), 'age': 52, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
         {'id': '@I04@', 'name': 'Beau /Biden/', 'gender': 'M', 'birthday': datetime.date(1969, 2, 3), 'age': 52, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}]
-  test_fam = [
-        {'id': '@F01@', 'married': datetime.date(2000, 6, 17), 'divorced': 'NA', 'husb_id': '@I01@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I02@', 'wife_name': 'Neilia /Hunter/', 'children': {'@I03@', '@I04@'}}]
-  
+  test_fam = {'id': '@F01@', 'married': datetime.date(2000, 6, 17), 'divorced': 'NA', 'husb_id': '@I01@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I02@', 'wife_name': 'Neilia /Hunter/', 'children': {'@I03@', '@I04@'}}
+ 
   # define test methods
   def test_check_male_last(self):
     self.assertTrue(user_story_16.checkMaleLastNames(self.test_fam, self.test_true))
