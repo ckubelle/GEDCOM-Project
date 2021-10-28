b330ee1a4e140895e5e7efefd12c271eb88024e6
amohan00 <arm198@pitt.edu>
2021-10-28 17:18:57 -0400

    finished testcases for us16

--- test_user_stories/test_user_story_16.py
+++ test_user_stories/test_user_story_16.py
@@ -6,7 +6,7 @@ import datetime
 from user_stories import user_story_16
 
 class TestUserStory16(unittest.TestCase):
-
+  # define objects to interact with during tests
   test_true = [
         {'id': '@I01@', 'name': 'Joseph /Biden/', 'gender': 'M', 'birthday': datetime.date(1942, 11, 20), 'age': 78, 'alive': True, 'death': 'NA', 'child': set(), 'spouse': {'@F01@'}},
         {'id': '@I02@', 'name': 'Neilia /Hunter/', 'gender': 'F', 'birthday': datetime.date(1942, 11, 28), 'age': 78, 'alive': True, 'death': 'NA', 'child': set(), 'spouse': {'@F01@'}},
@@ -20,9 +20,15 @@ class TestUserStory16(unittest.TestCase):
   test_fam = [
         {'id': '@F01@', 'married': datetime.date(2000, 6, 17), 'divorced': 'NA', 'husb_id': '@I01@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I02@', 'wife_name': 'Neilia /Hunter/', 'children': {'@I03@', '@I04@'}}]
   
+  # define test methods
   def test_check_male_last(self):
-    self.assertTrue(user_story_16.checkMaleLastNames)
+    self.assertTrue(user_story_16.checkMaleLastNames(self.test_fam, self.test_true))
+    self.assertFalse(user_story_16.checkMaleLastNames(self.test_fam, self.test_false))
+
   def test_is_male(self):
-    
-  def find_last(self):
-    self.assertTrue(
+    self.assertTrue(user_story_16.isMale('@I01@', self.test_true))
+    self.assertFalse(user_story_16.isMale('@I02@', self.test_true))
+  
+  def test_find_last(self):
+    self.assertEqual(user_story_16.findLastName('@I01@', self.test_true), "/Biden/")
+    self.assertNotEqual(user_story_16.findLastName('@I03@', self.test_false), "/Biden/")
