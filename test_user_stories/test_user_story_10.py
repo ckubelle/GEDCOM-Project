77e77f2f848917407d57e4d0f81f723bef648dc5
amohan00 <arm198@pitt.edu>
2021-10-28 17:43:29 -0400

    Create test_user_story_10.py

---
+++ test_user_stories/test_user_story_10.py
@@ -0,0 +1,17 @@
+# User Story 10 Test Class
+# Marriage should be at least 14 years after birth of both spouses
+
+import unittest
+import datetime
+from user_stories import user_story_10
+
+class TestUserStory10(unittest.TestCase):
+    # create test objects to interact with
+
+
+    # define test methods
+    def test_is_marr_before_14(self):
+
+    def test_diff_dates(self):
+
+    def test_get_birthday(self):
