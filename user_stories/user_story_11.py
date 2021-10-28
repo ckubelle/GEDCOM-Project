1a7a5f3a2ed1514bbd6b0926edeb08375de12937
amohan00 <arm198@pitt.edu>
2021-10-28 02:48:35 -0400

    created user_story_11.py

---
+++ user_stories/user_story_11.py
@@ -0,0 +1,6 @@
+# User Story 11 - Marriage should not occur 
+# during marriage to another spouse
+
+# return error statements if bigamy exists
+def isBigamy(indi_list, fam_list):
+    for fam in fam_list:
