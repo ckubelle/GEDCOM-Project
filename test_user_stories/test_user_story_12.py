4529ac1e87e78bcba3ac6deebbbc3c6cf4273c20
amohan00 <arm198@pitt.edu>
2021-10-28 17:45:26 -0400

    Create test_user_story_12.py

---
+++ test_user_stories/test_user_story_12.py
@@ -0,0 +1,21 @@
+# User Story 12 Test Class
+# Mother should be less than 60 years 
+# older than her children and father should be 
+# less than 80 years older than his children
+
+import unittest
+import datetime
+from user_stories import user_story_12
+
+class TestUserStory12(unittest.TestCase):
+    # create test objects to interact with
+
+
+    # define test methods
+    def test_check_parents_too_old(self):
+      
+    def test_get_youngest_bday(self):
+
+    def test_diff_dates(self):
+
+    def test_get_birthday(self):
