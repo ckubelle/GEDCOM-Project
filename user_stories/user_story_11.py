66245b730bc6f96cabe45137f3ed02f33c267831
amohan00 <arm198@pitt.edu>
2021-11-09 13:21:53 -0500

    finished isBigamy and added imports

--- user_stories/user_story_11.py
+++ user_stories/user_story_11.py
@@ -1,10 +1,34 @@
 # User Story 11 - Marriage should not occur 
 # during marriage to another spouse
 
+from datetime import date, datetime
+
 # return error statements if bigamy exists
 def isBigamy(indi_list, fam_list):
+    errorStatements = []
     for fam in fam_list:
-        
+        if checkBigamy(indi_list, fam_list, fam):
+            errorStatements.append("Error US11: Marriage in Family %s occurred during marriage to another spouse." % (fam["id"]))
+    return errorStatements
+
+# return true if bigamy exists in given family
+def checkBigamy(indi_list, fam_list, fam):
+    # check if they are married
+    if fam["married"] == "NA":
+        return False
+    else:
+        # get marriage start date
+        currentMarr = fam["married"]
+        # check if spouses are in other marriages
+        for fam2 in fam_list:
+            if fam2 != fam:
+                if fam2["husb_id"] == fam["husb_id"] or fam2["wife_id"] == fam["wife_id"]:
+                    # if start of current marriage occured before end of other, return true
+                    if (currentMarr < getEndMarr(indi_list, fam2)):
+                        return True
+                    else:
+                        return False 
+                    
 # return end of marriage date or date(3000, 1, 1) if none
 def getEndMarr(indi_list, fam):
     # if no divorce, endOfMarr is whichever spouse died first, or date(3000, 1, 1) for default
