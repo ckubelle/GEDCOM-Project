16fb6971344e4ee964febef25bebe9802597239c
amohan00 <arm198@pitt.edu>
2021-11-09 21:11:45 -0500

    Update user_story_20.py

--- user_stories/user_story_20.py
+++ user_stories/user_story_20.py
@@ -1,2 +1,11 @@
 # User Story 20 - Aunts and uncles should 
 # not marry their nieces or nephews
+
+
+# return error statements if marriage is between an aunt/uncle and neice/nephew
+def isAuntOrUnc(indi_list, fam_list):
+    errorStatements = []
+    for fam in fam_list:
+        if checkAuntorUnc(indi_list, fam):
+            errorStatements.append("Error US20: Spouse in Family %s is married to their neice or nephew." % (fam["id"]))
+    return errorStatements
