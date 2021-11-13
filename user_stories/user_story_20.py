adac7f83ff2fdbb43fd78a6ad0fd856688d0448b
amohan00 <arm198@pitt.edu>
2021-11-13 13:07:38 -0500

    overhaul of us_20 to fix bugs

--- user_stories/user_story_20.py
+++ user_stories/user_story_20.py
@@ -2,41 +2,63 @@
 # not marry their nieces or nephews
 
 # return error statements if marriage is between an aunt/uncle and neice/nephew
-def isAuntOrUnc(indi_list, fam_list):
+def isAuntUnc(indi_list, fam_list):
     errorStatements = []
     for fam in fam_list:
-        if checkAuntorUnc(indi_list, fam_list, fam):
+        if checkAuntUnc(indi_list, fam_list, fam):
             errorStatements.append("Error US20: Spouse in Family %s is married to their neice or nephew." % (fam["id"]))
     return errorStatements
 
-# return true if the given family marriage is between an aunt/uncle and neice/nephew
-def checkAuntorUnc(indi_list, fam_list, fam):
-    husbId = fam["husb_id"]
-    wifeId = fam["wife_id"]
-    # if a spouse's neice or nephew ID matches their spouse's ID, return true
-    for neice in getNeiceNeph(indi_list, fam_list, husbId):
-        if neice[id] == wifeId:
+# return True if given fam contains marriage between aunt/uncle and neice/nephew
+def checkAuntUnc(indi_list, fam_list, fam):
+    if fam['married'] == 'NA':
+        return False
+    husb_id = fam['husb_id']
+    wife_id = fam['wife_id']
+    husb_ext = getAuntUnc(indi_list, fam_list, husb_id)
+    wife_ext = getAuntUnc(indi_list, fam_list, wife_id)
+    for unc in husb_ext:
+        if unc == wife_id:
             return True
-    for nephew in getNeiceNeph(indi_list, fam_list, wifeId):
-        if nephew[id] == husbId:
+    for unc in wife_ext:
+        if unc == husb_id:
             return True
     return False
 
-
-# return given indi's neices and nephews
-def getNeiceNeph(indi_list, fam_list, indi):
-    neph = set()
+# return given ind's aunts and uncles
+def getAuntUnc(indi_list, fam_list, indi_id):
+    auntsUncs = set()
+    given_fam = ''
+    mom_id = ''
+    dad_id = ''
+    mom_fam = ''
+    dad_fam = ''
+    # find given ind's family
     for ind in indi_list:
-        # find given indi
-        if ind["id"] == indi:
-            family = ind["child"]
-            # find indi's family
-            for fam in fam_list:
-                if fam["id"] == family:
-                    for sibling in fam["children"]:
-                        # find all siblings except given indi
-                        if sibling["id"] != ind["id"]:
-                            # append sibling's children to neph
-                            for child in sibling["children"]:
-                                neph.append(child)
-    return neph
+        if ind['id'] == indi_id:
+            for fam in ind['child']:
+                given_fam = fam
+    # find given ind's parents
+    for fam in fam_list:
+        if fam["id"] == given_fam:
+            dad_id = fam['husb_id']
+            mom_id = fam['wife_id']
+    # find parents' families
+    for ind in indi_list:
+        if ind['id'] == dad_id:
+            for fam in ind['child']:
+                dad_fam = fam
+        elif ind['id'] == mom_id:
+            for fam in ind['child']:
+                mom_fam = fam
+    # find parents siblings
+    for fam in fam_list:
+        if fam['id'] == dad_fam:
+            for child in fam['children']:
+                if child != dad_id:
+                    auntsUncs.add(child)
+        elif fam['id'] == mom_fam:
+            for child in fam['children']:
+                if child != mom_id:
+                    auntsUncs.add(child)
+    return auntsUncs
