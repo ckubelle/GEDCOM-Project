34a69e249b893ec468e8df1baf407f9fe4f49aeb
amohan00 <arm198@pitt.edu>
2021-11-13 14:11:16 -0500

    finished test us_11

--- test_user_stories/test_user_story_11.py
+++ test_user_stories/test_user_story_11.py
@@ -1,5 +1,6 @@
 # User Story 11 Test Class
 # No Bigamy
+
 import unittest
 from datetime import date
 from user_stories import user_story_11
@@ -14,11 +15,16 @@ class TestUserStory11(unittest.TestCase):
         {'id': '@I05@', 'name': 'Kim /Biden/', 'gender': 'F', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': False, 'death': date(1960, 2, 20), 'child': {'@F01@'}, 'spouse': set()}]
     test_none = {'id': '@F01@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I01@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I02@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
     test_div = {'id': '@F02@', 'married': date(1950, 6, 17), 'divorced': date(1970, 6, 17), 'husb_id': '@I01@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I02@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
-    test_husb = {'id': '@F03@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I04@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I02@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
-    test_wife = {'id': '@F03@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I01@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I05@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
-    test_famlist = [test_none, test_div, test_husb, test_wife]
+    test_husb = {'id': '@F03@', 'married': date(1970, 6, 17), 'divorced': 'NA', 'husb_id': '@I04@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I02@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
+    test_wife = {'id': '@F04@', 'married': date(1970, 6, 17), 'divorced': 'NA', 'husb_id': '@I01@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I05@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
+    test_famlist = [test_none, test_husb, test_wife]
+    # define test methods
+    def test_check_Bigamy(self):
+        self.assertFalse(user_story_11.checkBigamy(self.test_indi, self.test_famlist, self.test_none))
+        self.assertTrue(user_story_11.checkBigamy(self.test_indi, self.test_famlist, self.test_husb))
+        self.assertTrue(user_story_11.checkBigamy(self.test_indi, self.test_famlist, self.test_wife))
     
-     def test_get_End_Marr(self):
+    def test_get_End_Marr(self):
         self.assertEqual(user_story_11.getEndMarr(self.test_indi, self.test_none), date(3000, 1, 1))
         self.assertEqual(user_story_11.getEndMarr(self.test_indi, self.test_div), date(1970, 6, 17))
         self.assertEqual(user_story_11.getEndMarr(self.test_indi, self.test_husb), date(1960, 2, 20))
