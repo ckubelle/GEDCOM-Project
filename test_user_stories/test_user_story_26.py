f98bcc5a87553da759c1c00228802be17fcae33b
Leena Domadia <leena.domadia2018@gmail.com>
2021-11-17 21:58:17 -0500

    updated test cases

--- test_user_stories/test_user_story_26.py
+++ test_user_stories/test_user_story_26.py
@@ -3,24 +3,30 @@
 
 import unittest
 
-from user_stories.user_story_26 import isCorrespondingChild,  isCorrespondingSpouse
+from user_stories.user_story_26 import isChildInFamChildren, isFamInIndiChild, isSpouseInFam, isFamInIndiSpouse
 
 class TestUserStory26(unittest.TestCase):
 
-    def test_not_corresponding_child(self):
-        self.assertFalse(isCorrespondingChild('I1', 'F1', 'F1', []))
-    
-    def test_different_fam_id_corresponding_child(self):
-        self.assertTrue(isCorrespondingChild('I1', 'F1', 'F2', ['I1']))
+    def test_is_child_in_fam_children(self):
+        self.assertTrue(isChildInFamChildren('I1', ['I2', 'I1', 'I3']))
 
-    def test_is_corresponding_child(self):
-        self.assertTrue(isCorrespondingChild('I1', 'F1', 'F1', ['I2', 'I1', 'I3']))
+    def test_is_not_child_in_fam_children(self):
+        self.assertFalse(isChildInFamChildren('I4', ['I2', 'I1', 'I3']))
 
-    def test_not_corresponding_spouse(self):
-        self.assertFalse(isCorrespondingSpouse('I1', 'F1', 'F1', 'I2', 'I3'))
-    
-    def test_different_fam_id_corresponding_spouse(self):
-        self.assertTrue(isCorrespondingSpouse('I1', 'F1', 'F2', 'I2', 'I1'))
+    def test_is_fam_in_indi_child(self):
+        self.assertTrue(isFamInIndiChild('F1', {'F1'}))
 
-    def test_is_corresponding_spouse(self):
-        self.assertTrue(isCorrespondingSpouse('I1', 'F1', 'F1', 'I2', 'I1'))
+    def test_is_not_fam_in_indi_child(self):
+        self.assertFalse(isFamInIndiChild('F1', {'F2'}))
+
+    def test_is_spouse_in_fam(self):
+        self.assertTrue(isSpouseInFam('I1', 'I1', 'I2'))
+
+    def test_is_not_spouse_in_fam(self):
+        self.assertFalse(isSpouseInFam('I1', 'I3', 'I2'))
+
+    def test_is_fam_in_indi_spouse(self):
+        self.assertTrue(isFamInIndiChild('F1', {'F1', 'F2', 'F3'}))
+
+    def test_is_not_fam_in_indi_spouse(self):
+        self.assertFalse(isFamInIndiChild('F1', {'F2', 'F3', 'F4'}))
--- user_stories/user_story_26.py
+++ user_stories/user_story_26.py
@@ -2,24 +2,6 @@
 # User Story 26 - Corresponding Entries
 # I pledge my Honor that I have abided by the Stevens Honor System - Leena Domadia
 
-def isCorrespondingChild(indi_indi_id, indi_fam_id, fam_fam_id, fam_children):
-    if indi_fam_id == fam_fam_id:
-        if indi_indi_id in fam_children:
-            return True
-        else:
-            return False
-    # if id's are not the same, ignore
-    return True
-
-def isCorrespondingSpouse(indi_indi_id, indi_fam_id, fam_fam_id, fam_husb_id, fam_wife_id):
-    if indi_fam_id == fam_fam_id:
-        if indi_indi_id == fam_husb_id or indi_indi_id == fam_wife_id:
-            return True
-        else:
-            return False
-    # if id's are not the same, ignore
-    return True
-
 def isChildInFamChildren(indi_id, fam_children):
     if indi_id in fam_children:
         return True
@@ -46,28 +28,6 @@ def isFamInIndiSpouse(fam_id, indi_spouse):
 
 def correspondingEntries(indi_list, fam_list):
     error_statements = []
-    # # traversing indi_list for correctness
-    # for indi in indi_list:
-    #     if indi['child'] != 'NA':
-    #         for fam_id in indi['child']:
-    #             for fam in fam_list:
-    #                 if not isCorrespondingChild(indi['id'], fam_id, fam['id'], fam['children']):
-    #                     error_statements.append(f"Error US26: In the Individuals table, {indi['name']} ({indi['id']}) is listed as a child in {fam_id}, but isn't listed as a child in {fam['id']} in the Family table")
-    #     if indi['spouse'] != 'NA':
-    #         for fam_id in indi['spouse']:
-    #             for fam in fam_list:
-    #                 if not isCorrespondingSpouse(indi['id'], fam_id, fam['id'], fam['husb_id'], fam['wife_id']):
-    #                     error_statements.append(f"Error US26: In the Individuals table, {indi['name']} ({indi['id']}) is listed as a spouse in {fam_id}, but isn't listed as a spouse in {fam['id']} in the Family table")
-    # # traversing fam_list for correctness
-    # for fam in fam_list:
-    #     if fam['children'] != 'NA':
-    #         for indi_id in fam['children']:
-    #             for indi in indi_list:
-    #                 if indi['child'] != 'NA':
-    #                     for fam_id in indi['child']:
-    #                         if not isCorrespondingChild(indi['id'], )
-    # return error_statements
-
     for indi in indi_list:
         for fam in fam_list:
             indi_id = indi['id']
