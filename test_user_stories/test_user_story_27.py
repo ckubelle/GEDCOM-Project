1fee078212bef6a6e1161d327e7a845d26ebabcd
amohan00 <arm198@pitt.edu>
2021-12-02 04:26:07 -0500

    Update test_user_story_27.py

--- test_user_stories/test_user_story_27.py
+++ test_user_stories/test_user_story_27.py
@@ -7,12 +7,8 @@ from user_stories import user_story_27
 
 class TestUserStory27(unittest.TestCase):
     # create test objects to interact with
-    test_indi = [
-        {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
-        {'id': '@I02@', 'name': 'Kim /Hunter/', 'gender': 'F', 'birthday': "NA", 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
-        {'id': '@I03@', 'name': 'Tim /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': "NA", 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()}]
     test_true = {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
-    test_wrong = {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 90, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
+    test_wrong = {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 80, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
     test_date = {'id': '@I02@', 'name': 'Kim /Hunter/', 'gender': 'F', 'birthday': "NA", 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
     test_age = {'id': '@I03@', 'name': 'Tim /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': "NA", 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()}
         
