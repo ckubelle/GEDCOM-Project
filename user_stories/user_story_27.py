88a4ad9ac35fdc7858fdfb1b7c8735baff05cff1
amohan00 <arm198@pitt.edu>
2021-12-01 23:16:58 -0500

    us27 first try

--- user_stories/user_story_27.py
+++ user_stories/user_story_27.py
@@ -2,7 +2,21 @@
 # Include person's current age when listing individuals
 
 # return error statements if individual is missing current age
-def isIncludeAge(indi_list, fam_list):
+def isIncludeAge(indi_list):
+    errorStatements = []
+    for ind in indi_list:
+        if checkIncludeAge(indi_list, ind):
+            errorStatements.append("Error US27: Individual %s does not have a current age" % (ind["id"]))
+    return errorStatements
 
 # return true if given individual's age is included
 def checkIncludeAge(indi_list, fam_list, indi):
+    for ind in indi_list:
+        if ind['id'] == indi['id']:
+            # if ind has no birthday, return false
+            if ind['birthday'] == "NA":
+                return False
+            # else, if ind still has no age, return false
+            elif ind['age'] == "NA":
+                return False
+    return True
