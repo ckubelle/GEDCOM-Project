c07bf7409f3a8dbbf90b666b68d1bdd8d433957c
amohan00 <arm198@pitt.edu>
2021-12-02 03:04:44 -0500

    finished us27 tests

--- test_user_stories/test_user_story_27.py
+++ test_user_stories/test_user_story_27.py
@@ -8,18 +8,15 @@ from user_stories import user_story_27
 class TestUserStory27(unittest.TestCase):
     # create test objects to interact with
     test_indi = [
-        {'id': '@I01@', 'name': 'Joseph /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': {'@F03@'}},
-        {'id': '@I02@', 'name': 'Neilia /Hunter/', 'gender': 'F', 'birthday': date(1962, 2, 28), 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': {'@F03@'}},
-        {'id': '@I03@', 'name': 'James /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': 22, 'alive': True, 'death': 'NA', 'child': {'@F03@'}, 'spouse': set()},
-        {'id': '@I04@', 'name': 'Beau /Biden/', 'gender': 'M', 'birthday': date(2014, 2, 3), 'age': 7, 'alive': True, 'death': 'NA', 'child': {'@F03@'}, 'spouse': set()},
-        {'id': '@I05@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
-        {'id': '@I06@', 'name': 'Kim /Hunter/', 'gender': 'F', 'birthday': date(1962, 2, 28), 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
-        {'id': '@I07@', 'name': 'Tim /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': 22, 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()},
-        {'id': '@I08@', 'name': 'Sim /Biden/', 'gender': 'M', 'birthday': date(2014, 2, 3), 'age': 7, 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()}]
-    test_false = {'id': '@F01@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I20@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I21@', 'wife_name': 'Neilia /Hunter/', 'children': {'@I01@', '@I05@', '@I06@'}}
-    test_true = {'id': '@F04@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I03@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I07@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
-   
-    # define test methods
-    def test_is_Include_Age(self):
-
+        {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
+        {'id': '@I02@', 'name': 'Kim /Hunter/', 'gender': 'F', 'birthday': "NA", 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
+        {'id': '@I03@', 'name': 'Tim /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': "NA", 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()}]
+    test_true = {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
+    test_date = {'id': '@I02@', 'name': 'Kim /Hunter/', 'gender': 'F', 'birthday': "NA", 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
+    test_age = {'id': '@I03@', 'name': 'Tim /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': "NA", 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()}
+ 
     def test_check_Include_Age(self):
+        self.assertTrue(user_story_27.checkIncludeAge(self.test_indi, self.test_true))
+        self.assertFalse(user_story_27.checkIncludeAge(self.test_indi, self.test_date))
+        self.assertFalse(user_story_27.checkIncludeAge(self.test_indi, self.test_age))
+        
