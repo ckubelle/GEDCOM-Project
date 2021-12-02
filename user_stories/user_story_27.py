65533876694256e3803825bb636ca05c31db482b
amohan00 <arm198@pitt.edu>
2021-12-02 05:58:18 -0500

    us27 death bug fix

--- user_stories/user_story_27.py
+++ user_stories/user_story_27.py
@@ -12,12 +12,17 @@ def isIncludeAge(indi_list):
             errorStatements.append("Error US27: Individual %s does not have their current age listed" % (ind["id"]))
     return errorStatements
 
+# return false if ind is missing current age 
 def checkIncludeAge(indi):
     if indi['birthday'] == "NA" or indi['age'] == "NA":
         return False
     else:
+        # check if age is current or correct
         today = date.today()
-        diff = relativedelta.relativedelta(today, indi['birthday']).years
+        if indi["death"] == "NA":
+            diff = relativedelta.relativedelta(today, indi['birthday']).years
+        else:
+            diff = relativedelta.relativedelta(indi['death'], indi['birthday']).years
         if (diff != indi['age']):
             return False
         else:
