1c4023cda1db37c3dd58898807929dcc46daa6bf
amohan00 <arm198@pitt.edu>
2021-12-01 23:09:26 -0500

    Create user_story_27.py

---
+++ user_stories/user_story_27.py
@@ -0,0 +1,8 @@
+# User Story 27
+# Include person's current age when listing individuals
+
+# return error statements if individual is missing current age
+def isIncludeAge(indi_list, fam_list):
+
+# return true if given individual's age is included
+def checkIncludeAge(indi_list, fam_list, indi):
