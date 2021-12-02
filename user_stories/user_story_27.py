80db03147df50637193c921b30450e632dd92f81
amohan00 <arm198@pitt.edu>
2021-12-02 05:59:11 -0500

    us27 final bug fix

--- user_stories/user_story_27.py
+++ user_stories/user_story_27.py
@@ -8,7 +8,7 @@ from dateutil import relativedelta
 def isIncludeAge(indi_list):
     errorStatements = []
     for ind in indi_list:
-        if checkIncludeAge(ind):
+        if not checkIncludeAge(ind):
             errorStatements.append("Error US27: Individual %s does not have their current age listed" % (ind["id"]))
     return errorStatements
 
