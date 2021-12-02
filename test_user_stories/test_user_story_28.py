a21441eeb3d735cd9db0ec5fa38ba46f27ff9ac2
amohan00 <arm198@pitt.edu>
2021-12-02 12:25:51 -0500

    update test us28

--- test_user_stories/test_user_story_28.py
+++ test_user_stories/test_user_story_28.py
@@ -5,7 +5,7 @@ import unittest
 from datetime import date
 from user_stories import user_story_28
 
-class TestUserStory20(unittest.TestCase):
+class TestUserStory28(unittest.TestCase):
     # create test objects to interact with
     test_indi = [
         {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
@@ -18,10 +18,11 @@ class TestUserStory20(unittest.TestCase):
     test_fam1 = {'id': '@F01@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I20@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I21@', 'wife_name': 'Neilia /Hunter/', 'children': {'@I01@', '@I02@', '@I03@'}}
     test_fam2 = {'id': '@F01@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I20@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I21@', 'wife_name': 'Neilia /Hunter/', 'children': {'@I01@'}}
     test_fam3 = {'id': '@F01@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I20@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I21@', 'wife_name': 'Neilia /Hunter/', 'children': set()}
+    test_fam4 = {'id': '@F01@', 'married': date(1950, 6, 17), 'divorced': 'NA', 'husb_id': '@I20@', 'husb_name': 'Joseph /Biden/', 'wife_id': '@I21@', 'wife_name': 'Neilia /Hunter/', 'children': "NA"}
     # define test methods
     def test_ordered_sibs(self):
         self.assertEqual(user_story_28.orderedSibs(self.test_indi, self.test_fam1), ['@I01@', '@I03@', '@I02@'])
         self.assertEqual(user_story_28.orderedSibs(self.test_indi2, self.test_fam1), ['@I02@', '@I01@', '@I03@'])
         self.assertEqual(user_story_28.orderedSibs(self.test_indi, self.test_fam2), ['@I01@'])
         self.assertEqual(user_story_28.orderedSibs(self.test_indi, self.test_fam3), [])
-        
+        self.assertEqual(user_story_28.orderedSibs(self.test_indi, self.test_fam4), [])
