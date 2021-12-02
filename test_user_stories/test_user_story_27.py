d2ced43720bb69faef7033a605e5bbaadd6be605
amohan00 <arm198@pitt.edu>
2021-12-02 04:25:24 -0500

    fixed test cases

--- test_user_stories/test_user_story_27.py
+++ test_user_stories/test_user_story_27.py
@@ -12,11 +12,14 @@ class TestUserStory27(unittest.TestCase):
         {'id': '@I02@', 'name': 'Kim /Hunter/', 'gender': 'F', 'birthday': "NA", 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()},
         {'id': '@I03@', 'name': 'Tim /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': "NA", 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()}]
     test_true = {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 91, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
+    test_wrong = {'id': '@I01@', 'name': 'Jim /Biden/', 'gender': 'M', 'birthday': date(1930, 2, 20), 'age': 90, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
     test_date = {'id': '@I02@', 'name': 'Kim /Hunter/', 'gender': 'F', 'birthday': "NA", 'age': 59, 'alive': True, 'death': 'NA', 'child': {'@F01@'}, 'spouse': set()}
     test_age = {'id': '@I03@', 'name': 'Tim /Biden/', 'gender': 'M', 'birthday': date(1999, 2, 3), 'age': "NA", 'alive': True, 'death': 'NA', 'child': {'@F02@'}, 'spouse': set()}
- 
-    def test_check_Include_Age(self):
-        self.assertTrue(user_story_27.checkIncludeAge(self.test_indi, self.test_true))
-        self.assertFalse(user_story_27.checkIncludeAge(self.test_indi, self.test_date))
-        self.assertFalse(user_story_27.checkIncludeAge(self.test_indi, self.test_age))
         
+   
+    # define test methods
+    def test_check_Include_Age(self):
+        self.assertTrue(user_story_27.checkIncludeAge(self.test_true))
+        self.assertFalse(user_story_27.checkIncludeAge(self.test_wrong))
+        self.assertFalse(user_story_27.checkIncludeAge(self.test_date))
+        self.assertFalse(user_story_27.checkIncludeAge(self.test_age))
