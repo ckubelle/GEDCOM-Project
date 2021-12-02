3f37231638691ae0f9afaa05c421d2400a78419e
amohan00 <arm198@pitt.edu>
2021-12-02 04:28:05 -0500

    import statements

--- user_stories/user_story_27.py
+++ user_stories/user_story_27.py
@@ -1,12 +1,15 @@
 # User Story 27
 # Include person's current age when listing individuals
+from datetime import date, datetime
+from dateutil import relativedelta
+
 
 # return error statements if individual is missing current age
 def isIncludeAge(indi_list):
     errorStatements = []
     for ind in indi_list:
         if checkIncludeAge(indi_list, ind):
-            errorStatements.append("Error US27: Individual %s does not have a current age" % (ind["id"]))
+            errorStatements.append("Error US27: Individual %s does not have their current age" % (ind["id"]))
     return errorStatements
 
 def checkIncludeAge(indi):
